23c5b11b752c0241f62370881e55171abfd35600
HinataKikuchi <haiao4041@gmail.com>
2021-05-12 19:11:23 +0900

    added subject file and example to day02

---
+++ day02/example_printf.c
@@ -0,0 +1,15 @@
+/*参照するライブラリを指定*/
+#include <stdio.h>
+
+/*戻り値の型　関数名　(引数の型)*/
+int main(void)
+{
+	/* printf */
+	/*%s = 文字列を出力*/
+	/*%d = 数字を10進数で出力*/
+	printf("My Name is = %s\nMy Age = %d", "Hinata Kikuchi", 17);
+
+	/*main関数の戻り値*/
+	return 0;
+}
+
